aa8208813928e94ff9b079e87b16f43bf396eb34
Yelyzaveta Shydenko <yelyzavetashydenko@MacBook-Pro-Kostiantyn.local>
2024-09-30 19:16:08 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Route, Routes } from "react-router-dom"; 
-import Layout from "./Components/Layout/Layout"; 
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Components/Layout/Layout";
 import ProfilePage from "./Pages/ProfilePage/ProfilePage";
-import ReviewsPage from "./Pages/ReviewsPage/ReviewsPage"; 
-import Login from "./Components/Login/Login"; 
-import "./App.css"; 
+import ReviewsPage from "./Pages/ReviewsPage/ReviewsPage";
+import Login from "./Components/Login/Login";
+import "./App.css";
 
 function App() {
   return (
@@ -11,12 +11,12 @@ function App() {
     <Routes>
       {/* Wrapping all routes with a common Layout component */}
       <Route path="/" element={<Layout />}>
+        {/* Route for the login page */}
+        <Route index path="/login" element={<Login />} />
         {/* Route for the profile page */}
         <Route path="/profile" element={<ProfilePage />} />
         {/* Route for the reviews page */}
         <Route path="/reviews" element={<ReviewsPage />} />
-        {/* Route for the login page */}
-        <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
   );
